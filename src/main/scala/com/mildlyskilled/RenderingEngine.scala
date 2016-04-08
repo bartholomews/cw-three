ccf53bb36b92fbacca6c63fbd5d14ca7e557aefd
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-09 00:24:50 +0100

    ray tracer - renderingengine needed more parameters to match coordinator

--- src/main/scala/com/mildlyskilled/RenderingEngine.scala
+++ src/main/scala/com/mildlyskilled/RenderingEngine.scala
@@ -6,6 +6,6 @@ import akka.actor.Actor.Receive
 /**
   * Created by annabel on 09/04/16.
   */
-class RenderingEngine extends Actor {
+class RenderingEngine(scene: Scene, counter: Counter, camera: Camera) extends Actor {
   override def receive: Receive = ???
 }
