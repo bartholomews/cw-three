cadaab634a9e55371e9452c1c390ae12a57a60d8
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-09 00:21:48 +0100

    ray tracer - added BLANK renderingengine as will need it as a worker

---
+++ src/main/scala/com/mildlyskilled/RenderingEngine.scala
@@ -0,0 +1,11 @@
+package com.mildlyskilled
+
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+/**
+  * Created by annabel on 09/04/16.
+  */
+class RenderingEngine extends Actor {
+  override def receive: Receive = ???
+}
