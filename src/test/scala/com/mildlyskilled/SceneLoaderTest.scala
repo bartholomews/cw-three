5165cca2996894b607c4795175270e76a4184669
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-04-09 10:42:12 +0100

    Tests for SceneLoader.

--- src/test/scala/com/mildlyskilled/SceneLoaderTest.scala
+++ src/test/scala/com/mildlyskilled/SceneLoaderTest.scala
@@ -1,16 +1,46 @@
 package com.mildlyskilled
 
+import java.io.FileNotFoundException
+
 import org.scalatest.FunSuite
 
 /**
   * Test class for scene loader.
   */
 class SceneLoaderTest extends FunSuite {
-  val file = "src/main/resources/input.dat"
+  val validConfig = "src/test/resources/valid.dat"
+  val noLights = "src/test/resources/noLights.dat"
+  val noObjects = "src/test/resources/noObjects.dat"
+  val falseConfig = "src/test/resources/falseConfig.dat"
+  var scene: Scene = _
 
-  test("Should load 2 lights and 3 objects form a file") {
-    val scene = SceneLoader.load(file)
-    assert(scene.lights.length == 2)
-    assert(scene.objects.length == 3)
+  test("Should load 6 lights and 6 objects form a file") {
+    scene = SceneLoader.load(validConfig)
+    assert(scene.lights.length == 6)
+    assert(scene.objects.length == 6)
+  }
+
+  test("Should throw an exception if file is missing") {
+    intercept[FileNotFoundException]{
+      scene = SceneLoader.load("missing.file")
+    }
+  }
+
+  test("Should throw an exception if no objects in file") {
+    intercept[RuntimeException]{
+      scene = SceneLoader.load(noObjects)
+    }
+  }
+
+  test("Should throw an exception if no lights in file") {
+    intercept[RuntimeException]{
+      scene = SceneLoader.load(noLights)
+    }
+  }
+
+  test("Should throw an exception if lights and objects present but false config") {
+    intercept[RuntimeException]{
+      scene = SceneLoader.load(falseConfig)
+    }
   }
 }
