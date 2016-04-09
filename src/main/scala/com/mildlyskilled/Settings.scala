51fac12e4bc7978e8b5af770db376aa57b5c58fe
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-04-09 09:53:27 +0100

    Values updated for tests

--- src/main/scala/com/mildlyskilled/Settings.scala
+++ src/main/scala/com/mildlyskilled/Settings.scala
@@ -4,12 +4,12 @@ package com.mildlyskilled
   * Class contains settings for rendering.
   */
 class Settings {
-  val antiAliasing = 1
-  val width = 100
-  val height = 100
+  val antiAliasing = 4
+  val width = 2000
+  val height = 2000
   val renderLineWidth = 10
 
   val ambient = 0.9f
   val backgroundColor = Colour.black
-  val renderNodeNumber = 10
+  val renderNodeNumber = 20
 }
