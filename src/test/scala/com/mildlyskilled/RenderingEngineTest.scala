6db7f8c175ea5e86a1be6dbd21e58c9f46e0eb14
rdrake01 <rustamdrake@gmail.com>
2016-04-09 03:29:38 +0100

    added the test for the rendering engine. to be complete

---
+++ src/test/scala/com/mildlyskilled/RenderingEngineTest.scala
@@ -0,0 +1,13 @@
+package com.mildlyskilled
+
+import org.scalatest.FunSuite
+import akka.testkit.TestActorRef
+
+/**
+  * Created by Rustam Drake on 4/9/16.
+  */
+class RenderingEngineTest extends FunSuite{
+  val renderingEngine = TestActorRef[RenderingEngine]
+
+
+}
