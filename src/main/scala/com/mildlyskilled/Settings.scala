685402ceab687d6b19067e8b0377f655bfb31266
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-04-09 09:33:30 +0100

    Lowered settings for manual testing

--- src/main/scala/com/mildlyskilled/Settings.scala
+++ src/main/scala/com/mildlyskilled/Settings.scala
@@ -4,9 +4,9 @@ package com.mildlyskilled
   * Class contains settings for rendering.
   */
 class Settings {
-  val antiAliasing = 6
-  val width = 800
-  val height = 600
+  val antiAliasing = 1
+  val width = 100
+  val height = 100
   val renderLineWidth = 10
 
   val ambient = 0.9f
