e14291f4a03d307eccc9d4f7d5f112d3612c0cdb
Annabel Jump <ajump02@dcs.bbk.ac.uk>
2016-04-10 02:07:45 +0100

    Added variables into test, passed them to TestActorRef instance of Coordinator

--- src/test/scala/com/mildlyskilled/CoordinatorTest.scala
+++ src/test/scala/com/mildlyskilled/CoordinatorTest.scala
@@ -13,13 +13,21 @@ class CoordinatorTest(_system: ActorSystem) extends TestKit(_system) with Implic
 
   def this() = this(ActorSystem("CoordinatorTest"))
 
+  val (inFile, outFile) = ("src/main/resources/input.dat", "output.png")
+  val settings = new Settings(800, 600, 10, 4, 0.6f, Colour.black, 10)
+  val counter = new Counter
+  val camera = new Camera(Vector.origin, 90f)
+  val scene = SceneLoader.load(inFile)
+
+  val image = new Image(settings.width, settings.height)
+
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
   "A Coordinator actor" must {
 
-    val testCoordinator = TestActorRef[Coordinator]
+    val testCoordinator = TestActorRef(new Coordinator(image, outFile, scene, settings, counter, camera))
     val realCoordinator = testCoordinator.underlyingActor
 
     "have a worker router" in {
